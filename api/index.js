e3c36c1fc5bb6bbd4bf6b149f5d9bd2f1b91bf6b
scottimariano <32655578+scottimariano@users.noreply.github.com>
2022-07-29 14:35:52 -0300

    PUERTO 4

--- api/index.js
+++ api/index.js
@@ -20,9 +20,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+const host = '0.0.0.0';
+const port = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-	server.listen(process.env.PORT || 3001, () => {
+	server.listen(port, host, () => {
 		console.log(`Server listening at ${process.env.PORT}`); // eslint-disable-line no-console
 	});
 });
