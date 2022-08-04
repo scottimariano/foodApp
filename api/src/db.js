dd9604b3d75601153bcdf3a9112999b6b6d4db9f
scottimariano <32655578+scottimariano@users.noreply.github.com>
2022-08-04 14:57:01 -0300

    promisified diet & types load

--- api/src/db.js
+++ api/src/db.js
@@ -70,47 +70,60 @@ sequelize.models = Object.fromEntries(capsEntries);
 const { Recipe, Diet, DishType } = sequelize.models;
 
 //cargamos las dietas en una primera instancia:
-const diets = [
-	'gluten free',
-	'vegetarian',
-	'ketogenic',
-	'lacto ovo vegetarian',
-	'vegan',
-	'pescatarian',
-	'paleolithic',
-	'primal',
-	'dairy free',
-	'fodmap friendly',
-	'whole30',
-];
+async function loadDiets() {
+	const diets = [
+		'gluten free',
+		'vegetarian',
+		'ketogenic',
+		'lacto ovo vegetarian',
+		'vegan',
+		'pescatarian',
+		'paleolithic',
+		'primal',
+		'dairy free',
+		'fodmap friendly',
+		'whole30',
+	];
 
-diets.map((d) =>
-	Diet.create({
-		where: { name: d },
-	})
-);
+	diets.map((d) =>
+		Diet.findOrCreate({
+			where: { name: d },
+		})
+	);
+	return Promise.all(diets);
+}
 
-const types = [
-	'main course',
-	'side dish',
-	'dessert',
-	'appetizer',
-	'salad',
-	'bread',
-	'breakfast',
-	'soup',
-	'beverage',
-	'sauce',
-	'marinade',
-	'fingerfood',
-	'snack',
-	'drink',
-];
+async function loadTypes() {
+	const types = [
+		'main course',
+		'side dish',
+		'dessert',
+		'appetizer',
+		'salad',
+		'bread',
+		'breakfast',
+		'soup',
+		'beverage',
+		'sauce',
+		'marinade',
+		'fingerfood',
+		'snack',
+		'drink',
+	];
 
-types.map((d) =>
-	DishType.create({
-		where: { name: d },
-	})
+	types.map((d) =>
+		DishType.findOrCreate({
+			where: { name: d },
+		})
+	);
+	return Promise.all(types);
+}
+
+Promise.all([loadDiets(), loadTypes()]).then(
+	(response) => {
+		console.log('Diets y types loaded OK');
+	},
+	(e) => console.log(e.message)
 );
 
 // Aca vendrian las relaciones
